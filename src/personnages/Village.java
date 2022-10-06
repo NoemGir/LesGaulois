9301838fd37751855e42ca29de7d772a637cefa3
gireaud noemie <grn4213a@ens.fsi>
2022-10-06 15:06:08 +0200

    Le village gaulois

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -3,17 +3,59 @@ package personnages;
 public class Village {
 	private String nom;
 	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
 	
-	public Village(String nom, Chef chef) {
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
-		this.chef = chef;
-	}
-	
-	public String getNom() {
-		return nom;
+		this.villageois = new Gaulois[nbVillageoisMaximum];
 	}
 	
 	public void setChef(Chef chef) {
 		this.chef = chef;
 	}
-}
+
+	public String getNom() {
+		return nom;
+	}
+	
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois] = gaulois ;
+		nbVillageois += 1 ;
+	}
+	
+	public Gaulois trouverHabitant(int numero) {
+		return villageois[numero-1];
+	}
+	
+	public void afficherVillageois() {
+		int i;
+		System.out.println("Dans " + getNom() + " du chef " + chef.getNom() + " vivent les légendaires gaulois :");
+		for (i=0; i<nbVillageois; i++) {
+			System.out.println(" - " + villageois[i].getNom() );
+		}
+	}
+	
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductibles",30);
+		
+//		Gaulois gaulois = village.trouverHabitant(30);
+//		aucun gaulois n'a été mit dans le village donc cette case est vide
+		
+		Chef abraracourcix = new Chef("Abraracourcix", 6,1, village);
+		village.setChef(abraracourcix);
+		
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		village.ajouterHabitant(asterix);
+		
+//		Gaulois gaulois = village.trouverHabitant(1);
+//		System.out.println(gaulois);
+//		On reçoit les informations de Asterix, qui est le premier gaulois villageois enregistré dans le village.
+		
+		Gaulois obelix = new Gaulois("Obélix", 25);
+		village.ajouterHabitant(obelix);
+		
+		village.afficherVillageois();
+	}
+	
+}	
